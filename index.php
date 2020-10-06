7970f008abbad32394ebc81bac279145a8a4bd40
kdeepanair <72278745+kdeepanair@users.noreply.github.com>
2020-10-06 18:21:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,13 @@
  <?php
   $webport = $_SERVER['RDS_PORT'];
   print "This is the webhostname $webport";
+  $dbconnection = mysql_connect($Database, $DBUser, $DBPassword) or die("Could not connect: " . mysql_error());  
+   if($dbconnection){
+      print "Connected to aurora successfully";
+    } else {
+      echo "Error creating table: " . mysql_error($dbconnection);
+    }   
+ mysql_close($dbconnection);
  ?>
  </body>
 </html>
